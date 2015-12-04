c5572b44d47f8727cf52063e7b236c3e63316baa
493238731 <493238731@qq.com>
2015-12-04 20:36:01 +0800

    Update Minimum Size Subarray Sum.cpp
    add O(n*lg n) algorithm

--- Minimum Size Subarray Sum.cpp
+++ Minimum Size Subarray Sum.cpp
@@ -1,3 +1,4 @@
+//O(2*n)
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
@@ -14,5 +15,40 @@ public:
             }
         }
         return result==INT_MAX?0:result;
+    }
+};
+
+
+//O(n*lg n) MLT overflow
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if(s==0||nums.empty())  return 0;
+        int n=nums.size();
+        int result=minSubArrayLen(nums,0,n-1,s);
+        return result;
+    }
+private:
+    int minSubArrayLen(vector<int> nums,int lo,int hi,int s){
+        if(lo==hi)  return nums[lo]>=s?1:INT_MAX/2;
+        int mid=lo+(hi-lo)/2;
+        int minLen=partition(nums,lo,mid,hi,s);
+        return min(min(minSubArrayLen(nums,lo,mid,s),minSubArrayLen(nums,mid+1,hi,s)),minLen);
+    }
+    int partition(vector<int> nums,int lo,int mid,int hi,int s){
+        int sum=0;
+        for(int i=lo;i<mid+1;++i)  sum+=nums[i];
+        int end=mid+1,start=lo;
+        int minLen=INT_MAX/2;
+        while(start<=mid&&end<=hi){
+            sum+=nums[end];
+            if(sum>=s){
+                while(start<=mid&&sum>=s)
+                    sum-=nums[start++];
+                minLen=min(minLen,end-start+2);
+            }
+            end++;
+        }
+        return minLen==INT_MAX/2?0:minLen;
     }
 };
