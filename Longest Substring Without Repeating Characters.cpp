d5d854caad935b9aa067109354532f8e09009459
493238731 <493238731@qq.com>
2015-12-04 18:35:21 +0800

    Update Longest Substring Without Repeating Characters.cpp
    add new method

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -20,3 +20,24 @@ public:
         return answer;
     }
 };
+
+// more eaisly
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector <int> hash(256,-1);
+        int answer=0;
+        int i=0,j=0,len=s.size();
+        if(len<=1)  return len;
+        while(i<len){
+            if(hash[s[i]]>=j){
+                answer=max(answer,i-j);
+                j=hash[s[i]]+1;
+            }//end if
+            hash[s[i]]=i;
+            i++;
+        }//end while
+        answer=max(answer,i-j);
+        return answer;
+    }
+};
